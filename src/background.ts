faed2b50f2b2c6c8c9140f27f3eaf812c118d429
amin <amin.roosta@outlook.com>
2023-08-12 19:01:22 -0400

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,24 @@
+import { BingChat } from "./bing-chat";
+
+// const BING_COOKIE = '';
+
+
+// async function polling() {
+//   const prompt = 'Write a poem about cats.'
+
+//   const api = new BingChat({ cookie: BING_COOKIE })
+//   console.log(prompt);
+
+//   await api.ready;
+//   await api.sendMessage(prompt, {
+//     onProgress: (partial) => {
+//       // console.clear()
+//       console.log(partial.text)
+//     }
+//   });
+
+//   // setTimeout(polling, 1000 * 30);
+// }
+
+// console.log("started ...");
+// setTimeout(polling, 1000 * 5);
